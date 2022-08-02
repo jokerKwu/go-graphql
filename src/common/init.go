520546359c015fd6d2e784c01d9ffad1b6a63129
RyanBreathings <ryan@breathings.co.kr>
2022-08-02 20:53:20 +0900

    echo 서버 셋팅

---
+++ src/common/init.go
@@ -0,0 +1,8 @@
+package common
+
+import "github.com/labstack/echo/v4"
+
+func InitHandler(e *echo.Echo) error {
+
+	return nil
+}
---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"main/common"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+
+	if err := common.InitHandler(e); err != nil {
+		fmt.Println(err)
+	}
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
