12420459070d682b2bc9ca7b9b6853e7b92d736a
RyanBreathings <ryan@breathings.co.kr>
2022-09-28 20:45:08 +0900

    go graphql todo test

--- src/graph/model/models_gen.go
+++ src/graph/model/models_gen.go
@@ -8,10 +8,11 @@ type NewTodo struct {
 }
 
 type Todo struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
-	Done bool   `json:"done"`
-	User *User  `json:"user"`
+	ID     string `json:"id"`
+	Text   string `json:"text"`
+	Done   bool   `json:"done"`
+	UserID string `json:"userId"`
+	User   *User  `json:"user"`
 }
 
 type User struct {
--- src/graph/resolver.go
+++ src/graph/resolver.go
@@ -1,7 +1,10 @@
 package graph
 
+import "main/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
-
-type Resolver struct{}
+type Resolver struct {
+	todos []*model.Todo
+}
